856d175e3195e8fb9ef4004cc8ef857249f9f8c4
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-16 16:08:45 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import "./app.css";
 function App() {
   const [peso, setPeso] = React.useState("");
   const [altura, setAltura] = React.useState("");
+  const [mensagem, setMensagem] = React.useState("");
 
   function calcular() {
     const alt = altura / 100;
     const imc = peso / (alt * alt);
-    alert(imc.toFixed(2));
+
+    if (imc < 18.6) {
+      setMensagem(
+        `Seu IMC: ${imc.toFixed(2)}.
+        Você está abaixo do peso ideal!`
+      );
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMensagem(`
+      Seu IMC: ${imc.toFixed(2)}.
+      Você está no peso ideal!`);
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMensagem(`
+      Seu IMC: ${imc.toFixed(2)}.
+      Você está acima do peso ideal!`);
+    } else if (imc > 34.9) {
+      setMensagem(`
+      Seu IMC: ${imc.toFixed(2)}.
+      Você está com obesidade!`);
+    }
   }
 
   return (
@@ -18,13 +37,13 @@ function App() {
 
       <div className="area-input">
         <input
-          type="text"
+          type="number"
           placeholder="Peso em kg. Ex: 90"
           value={peso}
           onChange={(event) => setPeso(event.target.value)}
         />
         <input
-          type="text"
+          type="number"
           placeholder="Altura em cm. Ex: 185"
           value={altura}
           onChange={(event) => setAltura(event.target.value)}
@@ -33,7 +52,9 @@ function App() {
         <button onClick={calcular}>Calcular</button>
       </div>
 
-      <h2>Seu IMC é de:</h2>
+      <div className="msg">
+        <h2>{mensagem}</h2>
+      </div>
     </div>
   );
 }
