c47d9872721af49c730dea8b8cada5afbf01c5e4
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-16 14:51:56 -0300

    app iniciado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>vamos calcular seu imc</span>
+
+      <div className="area-input">
+        <input type="text" placeholder="Peso em kg. Ex:90" />
+        <input type="text" placeholder="Altura em cm. Ex:1.85" />
+
+        <button>Calcular</button>
+      </div>
+
+      <h2>Seu IMC é de:</h2>
     </div>
   );
 }
