8366b100b0c59323b9da5909537ef207ea9b6ada
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-03-19 12:02:34 -0300

    adicionado novas opções

--- src/App.js
+++ src/App.js
@@ -10,23 +10,37 @@ function App() {
     const alt = altura / 100;
     const imc = peso / (alt * alt);
 
-    if (imc < 18.6) {
+    if (imc < 18.5) {
       setMensagem(
         `Seu IMC: ${imc.toFixed(2)}.
-        Você está abaixo do peso ideal!`
+
+        Abaixo do peso`
       );
-    } else if (imc >= 18.6 && imc < 24.9) {
+    } else if (imc >= 18.5 && imc <= 24.99) {
       setMensagem(`
       Seu IMC: ${imc.toFixed(2)}.
-      Você está no peso ideal!`);
-    } else if (imc >= 24.9 && imc < 34.9) {
+
+      Peso normal`);
+    } else if (imc >= 25 && imc <= 29.99) {
       setMensagem(`
       Seu IMC: ${imc.toFixed(2)}.
-      Você está acima do peso ideal!`);
-    } else if (imc > 34.9) {
+
+      Sobrepeso`);
+    } else if (imc >= 30 && imc <= 34.99) {
       setMensagem(`
       Seu IMC: ${imc.toFixed(2)}.
-      Você está com obesidade!`);
+
+      Obesidade Grau I`);
+    } else if (imc >= 35 && imc <= 39.99) {
+      setMensagem(`
+      Seu IMC: ${imc.toFixed(2)}.
+
+      Obesidade Grau II`);
+    } else {
+      setMensagem(`
+      Seu IMC: ${imc.toFixed(2)}.
+      
+      Obesidade Grau III ou Mórbida`);
     }
   }
 
